8533223c2bae2cdd87cf61e4aa0c52d52da083ea
Simon <98567864+Simon-Losier@users.noreply.github.com>
2023-08-16 22:52:25 -0300

    Broken trains

--- Assets/Assets/Scripts/worldGenerator.cs
+++ Assets/Assets/Scripts/worldGenerator.cs
@@ -9,7 +9,9 @@ public class worldGenerator : MonoBehaviour
     public GameObject gold;
     public GameObject steel;
     public GameObject rails;
-
+    public GameObject initRail; // We take the position, rotation and scale from the in world object to summon the others, so it must be within the level
+    public GameObject brokenRail;
+    
     public int length = 80;
 
     public int interval = 3;
@@ -21,7 +23,7 @@ public class worldGenerator : MonoBehaviour
 
     void generateWorld()
     {
-        // 
+        // Create rocks
         for (int x = 0; x < length; x += interval)
         {
             int verticalHeight = 10;
@@ -36,9 +38,7 @@ public class worldGenerator : MonoBehaviour
                 }
                 Vector3 position = new Vector3(x, 0, z);
                 
-                Debug.Log("Instace at: x:" + x + "z: " + z);
-                GameObject objectToSpawn = gold; // Default is gold
-
+                // Select gold or steel
                 if (Random.Range(0, 3) < 1)
                 {
                     Instantiate(steel, position, Quaternion.identity);
@@ -49,10 +49,24 @@ public class worldGenerator : MonoBehaviour
                 }
             }
         }
-    }
-    // Update is called once per frame
-    void Update()
-    {
         
+        // Rail procederal generation
+        int lengthOfRailModule = 3;
+        for (int x = 1; x < length; x++)
+        {
+            Vector3 position = new Vector3(initRail.transform.position.x + (x * lengthOfRailModule), initRail.transform.position.y,
+                initRail.transform.position.z);
+            int immuneRails = 5;
+            if (x>immuneRails && Random.Range(0, 10) > 5) 
+            {
+                Debug.Log("Broken rail summoned");
+                GameObject rail = Instantiate(brokenRail, position, initRail.transform.rotation);
+                rail.transform.localScale = initRail.transform.localScale;
+            }
+            else
+            {
+                Instantiate(initRail, position, initRail.transform.rotation);
+            }
+        }
     }
 }
